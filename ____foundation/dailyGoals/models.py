f2dd5cb96b91ac7b8757127e9071569d600208b2
waterifire <69126570+waterifire@users.noreply.github.com>
2022-04-22 21:03:54 +0200

    made models of daily Goals and notes

--- ____foundation/dailyGoals/models.py
+++ ____foundation/dailyGoals/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class DailyGoal(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=40, blank=True, null=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.name}"
+
+
+class Note(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=40, blank=True, null=True)
+    description = models.TextField(max_length=100, blank=True, null=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.name}"
